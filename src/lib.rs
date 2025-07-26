bab1b5d2ebe2727ce1e73f215ee2dca65aededae
Sharran <sharran.murali@gmail.com>
2025-07-26 21:43:42 +0530

    remove rust

--- src/lib.rs
+++
@@ -1,7 +0,0 @@
-pub mod error;
-pub mod cli;
-pub mod config;
-pub mod session;
-
-// Re-export our Result type for easy use
-pub use error::{Result, TermdError};
--- src/session/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod types;
-
-// Re-export the main types for easy use
-pub use types::{Session, SessionInfo, WindowInfo, WindowId};
