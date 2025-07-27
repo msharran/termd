45a368f3af181b97c1e7b51f8784d02cc04dc621
Sharran <sharran.murali@gmail.com>
2025-07-28 01:01:32 +0530

    clap: Use clap version compatible with zig 0.14.1

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,47 @@
-const std = @import("std");
 const clap = @import("clap");
+const std = @import("std");
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const allocator = gpa.allocator();
-    defer {
-        const deinit_status = gpa.deinit();
-        if (deinit_status == .leak) {
-            std.debug.print("Memory leak detected\n", .{});
-        }
-    }
+    defer _ = gpa.deinit();
 
-    const params = clap.parseParams(allocator, &.{ clap.Param.help() });
-    defer clap.freeParams(allocator, params);
+    // First we specify what parameters our program can take.
+    // We can use `parseParamsComptime` to parse a string into an array of `Param(Help)`.
+    const params = comptime clap.parseParamsComptime(
+        \\-h, --help             Display this help and exit.
+        \\-n, --number <usize>   An option parameter, which takes a value.
+        \\-s, --string <str>...  An option parameter which can be specified multiple times.
+        \\<str>...
+        \\
+    );
 
-    if (params.help) {
-        clap.printHelp(std.io.getStdOut().writer());
-        return;
-    }
+    // Initialize our diagnostics, which can be used for reporting useful errors.
+    // This is optional. You can also pass `.{}` to `clap.parse` if you don't
+    // care about the extra information `Diagnostics` provides.
+    var diag = clap.Diagnostic{};
+    var res = clap.parse(clap.Help, &params, clap.parsers.default, .{
+        .diagnostic = &diag,
+        .allocator = gpa.allocator(),
+    }) catch |err| {
+        // Report useful error and exit.
+        diag.report(std.io.getStdErr().writer(), err) catch {};
+        return err;
+    };
+    defer res.deinit();
+
+    // `clap.help` is a function that can print a simple help message. It can print any `Param`
+    // where `Id` has a `description` and `value` method (`Param(Help)` is one such parameter).
+    // The last argument contains options as to how `help` should print those parameters. Using
+    // `.{}` means the default options.
+    if (res.args.help != 0)
+        return clap.help(std.io.getStdErr().writer(), clap.Help, &params, .{});
+
+    if (res.args.number) |n|
+        std.debug.print("--number = {}\n", .{n});
+    for (res.args.string) |s|
+        std.debug.print("--string = {s}\n", .{s});
+    for (res.positionals[0]) |pos|
+        std.debug.print("{s}\n", .{pos});
 }
+
+
